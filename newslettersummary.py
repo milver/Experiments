76437cafb2e996232c3a4c555c1e86d12362c773
Milver Valenzuela <milver@users.noreply.github.com>
2024-04-30 09:14:36 -0400

    Update newslettersummary.py

--- newslettersummary.py
+++ newslettersummary.py
@@ -36,25 +36,26 @@ SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 # If no valid token found, we will create one.
 creds = None
 
-# The file token.pickle contains the user access token.
-# Check if it exists
 if os.path.exists('token.pickle'):
-
+    try:
 	# Read the token from the file and store it in the variable creds
 	with open('token.pickle', 'rb') as token:
 		creds = pickle.load(token)
-
-# If credentials are not available or are invalid, ask the user to log in.
+        creds.refresh(Request())
+    except google.auth.exceptions.RefreshError as error:
+        # if refresh token fails, reset creds to none.
+        creds = None
+        print(f'An error occurred: {error}')
+# If there are no (valid) credentials available, let the user log in.
 if not creds or not creds.valid:
-	if creds and creds.expired and creds.refresh_token:
-		creds.refresh(Request())
-	else:
-		flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-		creds = flow.run_local_server(port=0)
-
-	# Save the access token in token.pickle file for the next run
-	with open('token.pickle', 'wb') as token:
-		pickle.dump(creds, token)
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the access token in token.pickle file for the next run
+    with open('token.pickle', 'wb') as token:
+	pickle.dump(creds, token)
 
 from inspect import formatannotationrelativeto
 import base64
